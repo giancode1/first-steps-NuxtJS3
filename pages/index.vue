a0e0e3a35f9635f7a69fd6bd5dc018710d707cf2
giancode1 <gieg5c@gmail.com>
2022-05-12 19:33:37 -0500

    First commit, talwindcss + counter

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!!
+    </h1>
+    <div>
+        Counter: {{ counter }}
+        <button class="bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white"
+            @click="counter++">
+            +1
+        </button>
+
+    </div>
+</template>
+
+<script setup>
+const counter = useState('counter', () => 0);
+</script>
+
+<style scoped>
+</style>
